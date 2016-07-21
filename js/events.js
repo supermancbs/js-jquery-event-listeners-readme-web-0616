5c6ad93690600f8dc064423573f21dfd3e43e374
supermancbs <clark.sacktor@gmail.com>
2016-07-21 19:59:41 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,45 @@
 //define functions here
 
+function getIt() {
+  $('p').click(function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function(){
+    $(this).addClass("tasty");
+  })
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(e) {
+    // 'g' corresponds to 71
+    if (e.which === 71) {
+      alert("You pressed 'g'!")
+    }
+  })
+}
+
+function submitIt() {
+  $("form").on('submit',function() {
+    alert('Your form is going to be submitted now.');
+    return;
+  })
+}
+
+
+
 $(document).ready(function(){
 
 // call functions here
+
+getIt()
+
+frameIt()
+
+pressIt()
+
+submitIt()
 
 });
